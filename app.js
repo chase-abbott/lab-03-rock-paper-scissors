98984cdc9991365f21cccd86b978b60d08d0c648
chase-abbott <cabbott93@gmail.com>
2021-03-25 13:49:48 -0700

    import elements from html to js

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 // import functions and grab DOM elements
+import { didUserWin } from './test/utils.js';
 
+const rockInput = document.querySelector('#rock');
+const paperInput = document.querySelector('#paper');
+const scissorsInput = document.querySelector('#scissors');
+const resultsDiv = document.querySelector('#results-div');
+const winsDiv = document.querySelector('#wins-div');
+const lossesDiv = document.querySelector('#losses-div');
+const submit = document.querySelector('#submit');
+const reset = document.querySelector('#reset');
+const winsSpan = document.querySelector('#wins-span');
+const lossesSpan = document.querySelector('#losses-span');
+
+
+
+console.log(reset, winsSpan, lossesSpan);
 // initialize state
+let wins = 0;
+let total = 0;
+let computerThrow = Math.random();
 
 // set event listeners to update state and DOM
